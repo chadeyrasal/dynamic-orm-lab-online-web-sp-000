df98262826ac2c0c30d25fc7cf6abe494693ecb0
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-10 14:27:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,7 +52,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{table_name} WHERE #{column_names} = #{attribute}"
+    sql = "SELECT * FROM #{table_name} WHERE #{column_names} = '#{attribute}'"
     DB[:conn].execute(sql)
   end
 
